2300412d5da9ad4fbe06be2e123f9e5d950bc216
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-09 22:53:52 +0900

    Initial Commit

---
+++ Byosyoku/ByosyokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByosyokuApp.swift
+//  Byosyoku
+//
+//  Created by 櫻井絵理香 on 2024/04/09.
+//
+
+import SwiftUI
+
+@main
+struct ByosyokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Byosyoku/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Byosyoku
+//
+//  Created by 櫻井絵理香 on 2024/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
