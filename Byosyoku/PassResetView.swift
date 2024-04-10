e67bf270e5eb3df6a4ecc6b837d7a46f121e1828
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-10 22:01:13 +0900

    add spm of firebase

---
+++ Byosyoku/PassResetView.swift
@@ -0,0 +1,18 @@
+//
+//  PassReset.swift
+//  Byosyoku
+//
+//  Created by 櫻井絵理香 on 2024/04/10.
+//
+
+import SwiftUI
+
+struct PassResetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PassResetView()
+}
