4ffdf96a2aa9d484e6ab1bf42075ff58fcc691c3
orukahairuka <sakuraierika03301109@gmail.com>
2024-04-17 18:51:08 +0900

    Alamofireできた

--- Byosyoku/Controller/Jancode/YahooAPI/APIController.swift
+++ Byosyoku/Controller/Jancode/YahooAPI/APIController.swift
@@ -7,3 +7,18 @@
 
 import Foundation
 import Alamofire
+
+func APIController(date: String){
+    AF.request("https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch?appid=dj00aiZpPTZhMmg2bUpCUWVIQyZzPWNvbnN1bWVyc2VjcmV0Jng9Mzc-&jan_code=4909411084875&results=1").response { response in
+        do {
+            let decoder = JSONDecoder()
+            if let data = response.data {
+                let welcome = try decoder.decode(Welcome.self, from: data)
+                print("welocme: 🤩\(welcome)")
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+
+}
